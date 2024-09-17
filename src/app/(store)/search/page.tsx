6334319c5f2dd572d5e0addc0078cf89c8046bbe
eduardobertozi <webdu15@gmail.com>
2024-09-17 14:06:27 -0300

    ✨ feat: cria estrutura da pagina de busca dos produtos

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,3 +1,45 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function SearchPage() {
-  return <div>search</div>
+  return (
+    <div className="flex flex-col gap-2">
+      <p>
+        Resultads para: <span className="font-semibold">moletom</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        {Array.from({ length: 3 }, (_, i) => (
+          <Link
+            key={i}
+            href={`/product/moletom-never-stop-learning`}
+            className="group relative flex justify-center items-center rounded-lg bg-zinc-900 overflow-hidden"
+          >
+            <Image
+              src="/moletom-never-stop-learning.png"
+              className="group-hover:scale-105 transition-transform duration-500"
+              width={480}
+              height={480}
+              quality={100}
+              alt=""
+            />
+
+            <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">
+                Moletom Never Stop Learning
+              </span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {(129).toLocaleString('pt-BR', {
+                  style: 'currency',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                  currency: 'BRL',
+                })}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
