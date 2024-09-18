245c34e4dbf32dffbb5031c80c915daad5b22f0a
eduardobertozi <webdu15@gmail.com>
2024-09-18 00:39:05 -0300

    :sparkles: feat: cria custom commands para os testes do cypress

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -2,7 +2,7 @@ describe('search products', () => {
   it('should be able search for products', () => {
     cy.visit('/')
 
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.location('pathname').should('include', '/search')
     cy.location('search').should('include', '?q=moletom')
